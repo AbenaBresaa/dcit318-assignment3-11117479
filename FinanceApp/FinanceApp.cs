461550ec707390b43d613ca57b76271533f32546
AbenaBresaa <akbresaa@st.ug.edu.gh>
2025-08-07 02:27:11 +0000

    Add ITransactionProcessor interface and implementations

--- FinanceApp/FinanceApp.cs
+++ FinanceApp/FinanceApp.cs
@@ -2,3 +2,34 @@
 
 // Record for transaction
 public record Transaction(int Id, DateTime Date, decimal Amount, string Category);
+
+// Interface for transaction processors
+public interface ITransactionProcessor
+{
+    void Process(Transaction transaction);
+}
+
+// Implementation of different processors
+public class BankTransferProcessor : ITransactionProcessor
+{
+    public void Process(Transaction transaction)
+    {
+        Console.WriteLine($"[Bank Transfer] Processed {transaction.Amount:C} for {transaction.Category}");
+    }
+}
+
+public class MobileMoneyProcessor : ITransactionProcessor
+{
+    public void Process(Transaction transaction)
+    {
+        Console.WriteLine($"[Mobile Money] Processed {transaction.Amount:C} for {transaction.Category}");
+    }
+}
+
+public class CryptoWalletProcessor : ITransactionProcessor
+{
+    public void Process(Transaction transaction)
+    {
+        Console.WriteLine($"[Crypto Wallet] Processed {transaction.Amount:C} for {transaction.Category}");
+    }
+}
