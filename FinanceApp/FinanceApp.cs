00b1085bc2036ffaced92bed59ce895f05b29c03
AbenaBresaa <akbresaa@st.ug.edu.gh>
2025-08-07 02:35:32 +0000

    Add FinanceApp and Program main entry

--- FinanceApp/FinanceApp.cs
+++ FinanceApp/FinanceApp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 // Record for transaction
@@ -70,6 +71,46 @@ public sealed class SavingsAccount : Account
             Balance -= transaction.Amount;
             Console.WriteLine($"Transaction of {transaction.Amount:C} for {transaction.Category} applied. New Balance: {Balance:C}");
         }
+    }
+}
+// Main app class
+public class FinanceApp
+{
+    private List<Transaction> _transactions = new();
+
+    public void Run()
+    {
+        var savingsAccount = new SavingsAccount("ACC246", 1500m);
+
+        var t1 = new Transaction(1, DateTime.Now, 180m, "Groceries");
+        var t2 = new Transaction(2, DateTime.Now, 520m, "Utilities");
+        var t3 = new Transaction(3, DateTime.Now, 900m, "Entertainment");
+
+        var mobileMoney = new MobileMoneyProcessor();
+        var bankTransfer = new BankTransferProcessor();
+        var cryptoWallet = new CryptoWalletProcessor();
+
+        mobileMoney.Process(t1);
+        savingsAccount.ApplyTransaction(t1);
+        _transactions.Add(t1);
+
+        bankTransfer.Process(t2);
+        savingsAccount.ApplyTransaction(t2);
+        _transactions.Add(t2);
+
+        cryptoWallet.Process(t3);
+        savingsAccount.ApplyTransaction(t3);
+        _transactions.Add(t3);
+    }
+}
+
+// Entry point
+public class Program
+{
+    public static void Main()
+    {
+        var app = new FinanceApp();
+        app.Run();
     }
 }
 
