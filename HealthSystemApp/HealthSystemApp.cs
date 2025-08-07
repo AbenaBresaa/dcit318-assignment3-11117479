bb05f206870806a0d48ea069f3f188c260e6e469
AbenaBresaa <akbresaa@st.ug.edu.gh>
2025-08-07 03:00:25 +0000

    Add Main method to run and test the healthcare system

--- HealthSystemApp/HealthSystemApp.cs
+++ HealthSystemApp/HealthSystemApp.cs
@@ -115,4 +115,25 @@ public class HealthSystemApp
         }
     }
 }
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var app = new HealthSystemApp();
+        app.SeedData();
+        app.BuildPrescriptionMap();
+        app.PrintAllPatients();
+
+        Console.Write("\nEnter Patient ID to view prescriptions: ");
+        if (int.TryParse(Console.ReadLine(), out int id))
+        {
+            app.PrintPrescriptionsForPatient(id);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input.");
+        }
+    }
+}
+
 
